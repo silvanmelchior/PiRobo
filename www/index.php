dd4369ed4fe74b2b2c413872da4d597c93345617
Silvan Melchior <silvanmelchior@gmail.com>
2018-06-12 08:29:13 +0000

    bootstrap and fontawesome

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <title>PiRobo Start Page</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">PiRobo</a>      
+      <ul class="navbar-nav mr-auto">
+      </ul>
+      <form action="icecoder" target="_blank">
+        <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit"><i class="fas fa-external-link-alt"></i> Code Editor</button>
+      </form>
+      <form action="user_ctrl" target="_blank">
+        <button class="btn btn-outline-secondary my-2 my-sm-0 ml-2" type="submit"><i class="fas fa-external-link-alt"></i> Web Control</button>
+      </form>
+    </nav>
+
+    <h1>Hello, world!</h1>
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+  </body>
+</html>
