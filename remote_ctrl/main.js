1a815ddb50822e8fd8cc147ac9c4020200cca8c9
Silvan Melchior <silvanmelchior@gmail.com>
2018-08-10 15:16:53 +0200

    discovery number

--- remote_ctrl/main.js
+++ remote_ctrl/main.js
@@ -5,9 +5,10 @@ function discover() {
     if(this.readyState == 4 && this.status == 200) {
       data = JSON.parse(this.responseText);
       html = "<table border=1 cellpadding=3>";
-      html += "<tr><th>IP</th><th>SW Version</th><th>HW ID</th></tr>";
+      html += "<tr><th>Nr</th><th>IP</th><th>SW Version</th><th>HW ID</th></tr>";
       for(i=0; i<data.length; i++) {
         html += "<tr>";
+        html += "<td>" + (i+1) + "</td>";
         html += "<td><a href=\"http://" + data[i].ip + "/\">" + data[i].ip + "</a></td>";
         html += "<td>" + data[i].sw_version + "</td>";
         html += "<td>" + data[i].hw_id + "</td>";
